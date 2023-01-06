c07f26610cb49dcb7e219adf144395c2c6a25f5a
Logan <6226581+LoganAC34@users.noreply.github.com>
2023-01-06 10:03:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sqlite3
+from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
+
+
+def send_message(recipient='CADD-7', message="Hello World!"):
+    recipient = 'CADD-7'  # Server ip
+    port = 4000
+
+    mySocket = socket(AF_INET, SOCK_DGRAM)
+    mySocket.connect((recipient, port))
+
+    while True:
+        mySocket.send(b'cool')
+        break
+
+if __name__ == '__main__':
+    send_message('PyCharm')
+
